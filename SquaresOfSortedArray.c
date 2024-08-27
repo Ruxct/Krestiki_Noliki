b170572164a0584ed2dc85135a4e30a5741bddeb
Ruxct <149004928+Ruxct@users.noreply.github.com>
2024-08-27 11:57:37 +0300

    Add files via upload

---
+++ SquaresOfSortedArray.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* sortedSquares(int* nums, int numsSize, int* returnSize) {
+    int i, j, temp;
+    
+    int* result = (int*)malloc(numsSize * sizeof(int));
+  
+    for (i = 0; i < numsSize; i++) {
+        result[i] = nums[i] * nums[i];
+    }
+    
+    for (i = 0; i < numsSize - 1; ++i) {
+        for (j = 0; j < numsSize - 1 - i; ++j) {
+            if (result[j] > result[j + 1]) {
+                temp = result[j];
+                result[j] = result[j + 1];
+                result[j + 1] = temp;
+            }
+        }
+    }
+    
+    *returnSize = numsSize;
+    return result;
+}
+
+int main() {
+    // Ввод данных
+    int nums[] = {-7, -3, 2, 3, 11};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int returnSize;
+
+   
+    int* result = sortedSquares(nums, numsSize, &returnSize);
+
+    // Вывод результата
+    printf("Отсортированные квадраты: ");
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d ", result[i]);
+    }
+    printf("\n");
+
+    // Освобождение выделенной памяти
+    free(result);
+
+    return 0;
+}
